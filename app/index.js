52e485ba38f90dd12c0586feff508dbb9f471444
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-05-26 10:58:37 -0400

    configured babel, webpack and project setup

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello React!</h1>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
