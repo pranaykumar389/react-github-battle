34524811e3cfda472309df96af945e9f98b76b7c
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-05-28 14:00:42 -0400

    added react-router-dom and rendering all the routes specified to it.

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Popular from './Popular';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div className="container">
-                <Popular />
-            </div>
+            <Router>
+                <div className="container">
+                    <Nav />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/battle" component={Battle} />
+                        <Route path="/popular" component={Popular} />
+                        <Route render={() => <h3>Not Found!</h3>} />
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
