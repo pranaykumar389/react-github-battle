137bdb7b8471ba145caf422a8bac4d74787d014e
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2018-05-29 22:16:25 -0400

    created helper functions for fetching the data from github api using promises.

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,69 @@
 import axios from 'axios';
 
+const clientId = "CLIENT_ID";
+const secretId = "SECRET_ID";
+const params = `?client_id=${clientId}&client_secret=${secretId}`;
+
+function getProfile (username) {
+    return axios.get(`https://api.github.com/users/${username}${params}`)
+        .then(user => {
+            return user.data;
+        })
+        .catch(err => console.log('err: ', err));
+}
+
+function getRepos (username) {
+    return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+}
+
+function getStarCount (repos) {
+    return repos.data.reduce((count, repo) => {
+        return count + repo.stargazers_count;
+    }, 0);
+}
+
+function calculateScore (profile, repos) {
+    const followers = profile.followers;
+    const totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+function handleError (error) {
+    console.warn(error);
+    return null;
+}
+
+function getUserData (player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(data => {
+        const profile = data[0];
+        const repos = data[1];
+
+        return {
+            profile,
+            score : calculateScore(profile, repos)
+        }
+
+    }).catch(err => console.log('err: ', err));
+}
+
+function sortPlayers (players) {
+    return players.sort((a, b) => {
+        return b.score - a.score;
+    });
+}
+
 module.exports = {
+
+    battle: (players) => {
+        return axios.all(players.map(getUserData))
+            .then(sortPlayers)
+            .catch(handleError);
+    },
+
     fetchPopularRepos: (language) => {
         const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
         return axios.get(encodedURI)
